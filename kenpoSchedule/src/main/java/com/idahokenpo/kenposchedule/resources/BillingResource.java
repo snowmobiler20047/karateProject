f7339c20f63e04a87b3aced61bbed037dc9ad2d1
Korey <Korey@Korey-PC>
2017-07-03 17:01:41 -0600

    @GET added

--- kenpoSchedule/src/main/java/com/idahokenpo/kenposchedule/resources/BillingResource.java
+++ kenpoSchedule/src/main/java/com/idahokenpo/kenposchedule/resources/BillingResource.java
@@ -2,6 +2,7 @@ package com.idahokenpo.kenposchedule.resources;
 
 import com.idahokenpo.kenposchedule.billing.BillingUtils;
 import java.time.LocalDate;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
@@ -12,12 +13,14 @@ public class BillingResource
     BillingUtils billing = new BillingUtils();
     
     @Path("runall")
+    @GET
     public Response getBillingReport()
     {
 	return Response.ok(billing.runReport()).build();
     }
     
     @Path("run")
+    @GET
     public Response getBillingReport(@QueryParam("accountId") String accountId, @QueryParam("date") String dateString)
     {
 	LocalDate date = LocalDate.parse(dateString);
